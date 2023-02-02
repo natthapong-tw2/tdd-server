b2e3585b1332e0519096891315de7fdd4decfe0f
Nat-macbook <nattha.int@gmail.com>
2023-02-02 18:01:24 +0700

    Add more example test cases

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,10 +1,36 @@
 import supertest from "supertest"
 
 describe("index", () => {
-  describe("health-check", () => {
+  describe("GET /health-check", () => {
     it("should return 200", async () => {
       const actual = await supertest("http://localhost:8080")
         .get("/health-check")
+
+      expect(actual.status).toEqual(200)
+    })
+  })
+
+  describe("GET /users", () => {
+    it("should return 200", async () => {
+      const actual = await supertest("http://localhost:8080")
+        .get("/users")
+
+      expect(actual.status).toEqual(200)
+    })
+
+    it("should return list of users", async () => {
+      const actual = await supertest("http://localhost:8080")
+        .get("/users")
+
+      expect(actual.body).toEqual([])
+    })
+  })
+
+  describe("GET /shops", () => {
+    it("should return status code as 200", async () => {
+      const actual = await supertest("http://localhost:8080")
+        .get("/shops")
+
 
       expect(actual.status).toEqual(200)
     })
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from "express"
 
 const app = express()
-app.get("/health-check", (req, res) => {
-  res.status(200).send({})
-})
+app
+  .get("/health-check", (req, res) => {
+    res.status(200).send({})
+  })
+  .get("/users", (req, res) => {
+    res.status(200).send([])
+  })
+  .get("/shops", (req, res) => {
+    res.status(200).send([])
+  })
 
 app.listen(8080, () => {
   console.log("server started at http://localhost:8080")
